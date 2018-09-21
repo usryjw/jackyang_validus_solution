b40405966c85b13f80b5dd76f2e46e38c9cad5f1
usryjw <jackyang.ca@gmail.com>
2018-09-21 13:00:14 -0400

    commit
    remove some unused imports

--- src/main/java/com/validus/jyang/music/model/Artist.java
+++ src/main/java/com/validus/jyang/music/model/Artist.java
@@ -3,7 +3,6 @@ package com.validus.jyang.music.model;
 import java.util.ArrayList;
 import java.util.List;
 
-import javax.persistence.CascadeType;
 import javax.persistence.Entity;
 import javax.persistence.FetchType;
 import javax.persistence.JoinColumn;
